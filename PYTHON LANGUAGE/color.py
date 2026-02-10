30edfeaf7fead4fca9cd5bd4a94748ee3b0ba42a
Abhishek roy chowdhury <abhishekroychowdhury91@gmail.com>
2026-02-10 00:31:59 -0800

    This is the python program code second part.

---
+++ PYTHON LANGUAGE/color.py
@@ -0,0 +1,9 @@
+color = input ("Enter a color: ")
+if color == "red":
+    print("Stop")
+elif color == "Green":
+    print("go")
+elif color == "Yellow":
+    print("Look")
+else:
+    print("Invalid color!")
---
+++ PYTHON LANGUAGE/even_odd.py
@@ -0,0 +1,6 @@
+'''This is a simple program to check whether a number is even or odd.'''
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
